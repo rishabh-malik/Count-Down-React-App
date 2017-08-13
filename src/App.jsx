9efff8f04e21e9759a51d9cb3d6781d46e1faad6
rishabh-malik <rishabhmalik249@gmail.com>
2017-08-13 17:38:04 +0530

    structing the app

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,19 @@ import React, { Component } from 'react';
 class App extends Component {
     render() {
         return(
-        <div>Countdown Champ, App component</div>
+        <div>
+            <div>Countdown to December 25, 2017</div>
+             <div>
+                 <div>14 days</div>
+                 <div>10 hourss</div>
+                 <div>15 minutes</div>
+                 <div>20 seconds</div>
+             </div>  
+             <div> 
+             <input placeholder='new date'/>
+             <button>Submit</button>    
+             </div>
+        </div>
         )}
 }
 
