41328b91575e8f206b8fb1fa03b29d62830579ad
rishabh-malik <rishabhmalik249@gmail.com>
2017-08-13 17:45:46 +0530

    adding css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 //a react component - reuseable piece of UI
 import React, { Component } from 'react';
-
+import './App.css';
 class App extends Component {
     render() {
         return(
-        <div>
-            <div>Countdown to December 25, 2017</div>
+        //className is keyword used 
+        <div className="App">
+            <div className="App-title">Countdown to December 25, 2017</div>
              <div>
-                 <div>14 days</div>
-                 <div>10 hourss</div>
-                 <div>15 minutes</div>
-                 <div>20 seconds</div>
+                 <div className="Clock-days">14 days</div>
+                 <div className="Clock-hours">10 hourss</div>
+                 <div className="Clock-minutes">15 minutes</div>
+                 <div className="Clock-seconds">20 seconds</div>
              </div>  
              <div> 
              <input placeholder='new date'/>
